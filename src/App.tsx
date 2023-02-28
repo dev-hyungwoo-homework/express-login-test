d601a6e8737b089be467942cd0f923bb724567fc
rktnsinger <rktnsinger@gmail.com>
2023-02-28 18:29:36 +0900

    feat: 브라우저 확인 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 function App() {
   const userAgent = navigator.userAgent;
 
-  // if (userAgent.match(/Mozilla/)) {
-  // }
+  if (userAgent.indexOf("Android") >= 0) {
+    if (userAgent.indexOf("KAKAOTALK") >= 0) {
+      alert("현재 접속중인 브라우저는 카카오톡 인앱 브라우저입니다.");
+      window.location.href = "naversearchapp://default?version=5"
+    } else if (userAgent.indexOf("Instagram") >= 0) {
+      alert("현재 접속중인 브라우저는 인스타 인앱 브라우저입니다.");
+    } else if (userAgent.indexOf("NAVER") >= 0) {
+      alert("현재 접속중인 브라우저는 모바일 네이버 앱입니다.");
+    } else {
+      alert("현재 접속중인 브라우저는 모바일 크롬입니다.");
+      window.location.href = "naversearchapp://default?version=1"
+    }
+  }
 
   const handleButtonClick = async () => {
-    alert(userAgent)
-  }
+    alert(userAgent);
+  };
 
   return (
     <div>
       <h1>{"[POC] Express login App to Web"}</h1>
       <p>현재 로그인 브라우저 환경</p>
       <p>{userAgent}</p>
-      <button type="button" onClick={handleButtonClick}>로그인 하기</button>
+      <button type="button" onClick={handleButtonClick}>
+        로그인 하기
+      </button>
     </div>
   );
 }
