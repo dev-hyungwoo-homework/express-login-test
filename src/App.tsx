aae39df34c0ba062364a968101697b71c098788b
rktnsinger <rktnsinger@gmail.com>
2023-03-02 11:14:19 +0900

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
     window.location.href = "colavosalon://"
   } else if (userAgent.indexOf("CriOS") >= 0) {
     alert("현재 접속중인 브라우저는 IOS 크롬 브라우저입니다.");
-    window.location.href = "colavosalon://expresssign?id=123&browser=chrome&ip=123.12.3.2&browser_app_custom_url_scheme=naversearchapp://default?version=1" // 네이버 앱으로 정상 연결
+    // window.location.href = "colavosalon://expresssign?id=123&browser=chrome&ip=123.12.3.2&browser_app_custom_url_scheme=naversearchapp://default?version=1"
+    window.location.href = "naversearchapp://default?version=1" // 네이버 앱으로 정상 연결
   }
 
   const handleButtonClick = async () => {
