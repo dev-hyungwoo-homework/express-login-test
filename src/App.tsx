5b631e38b88c9dff7e79d9072b03ba35f2ca2df2
rktnsinger <rktnsinger@gmail.com>
2023-03-02 17:16:37 +0900

    커스텀URL스킴 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
 function App() {
   const userAgent = navigator.userAgent;
+  const url: string = process.env.REACT_APP_CUSTOM_URL_SCHEME as string
 
   if (userAgent.indexOf("KAKAOTALK") >= 0) {
     alert("현재 접속중인 브라우저는 카카오톡 인앱 브라우저입니다.");
-    // window.location.href = "naversearchapp://default?version=5" // 네이버 앱으로 정상 연결
-    window.location.href = "colavosalon://"
+    window.location.href = "naversearchapp://default?version=1" // 네이버 앱으로 정상 연결
   } else if (userAgent.indexOf("CriOS") >= 0) {
     alert("현재 접속중인 브라우저는 IOS 크롬 브라우저입니다.");
-    // window.location.href = "colavosalon://expresssign?id=123&browser=chrome&ip=123.12.3.2&browser_app_custom_url_scheme=naversearchapp://default?version=1"
-    window.location.href = "naversearchapp://default?version=1" // 네이버 앱으로 정상 연결
+    window.location.href = url
   }
 
   const handleButtonClick = async () => {
-    alert(userAgent);
+    if (userAgent.indexOf("CriOS") >= 0) {
+      window.location.href = url
+    } else {
+      alert(`미지원 기기 : ${userAgent}`);
+    }
   };
 
   return (
     <div>
       <h1>{"[POC] Express login App to Web"}</h1>
-      <p>현재 로그인 브라우저 환경</p>
+      <p>현재 로그인 브라우저 환경 : </p>
       <p>{userAgent}</p>
       <button type="button" onClick={handleButtonClick}>
-        로그인 하기
+        콜라보 살롱 앱으로 계속하기
       </button>
     </div>
   );
